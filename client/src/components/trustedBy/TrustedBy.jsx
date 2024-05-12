06af21ae6e6e020ef99c9d6d624f697969f98d3b
AnishAravindan5 <aravindananish2005@gmail.com>
2024-05-12 14:20:39 +0530

    Add files via upload

---
+++ client/src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TrustedBy.scss";
+
+const TrustedBy = () => {
+  return (
+    <div className="trustedBy">
+      <div className="container">
+        <span>Trusted by:</span>
+        <img src="/img/microsoft_logo.png" alt="" />
+        <img src="/img/angel_hack.png" alt="" />
+        <img src="/img/stackup_logo.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default TrustedBy;
